7cd07f9aa45398c140f5161fb7fc38b4bb836c44
akashmelkeri-incentius <akash.melkeri@incentius.com>
2023-03-29 11:52:08 +0530

    initial

---
+++ quasar_v1/src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    
+    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+    My name is Akash Melkeri. I am a fullstack developer. I like to explore different sectors of tech.
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
---
+++ quasar_v1/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix:'tw-',
+  content: ["./src/**/*.{vue,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
